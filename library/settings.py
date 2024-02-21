8c6104b59d8484f7d7c84aa142f195fa0b8db6c8
Shakhanov Amir <amirsahanov2@gmail.com>
2024-02-21 18:47:06 +0600

    Update settings.py

--- library/settings.py
+++ library/settings.py
@@ -77,11 +77,14 @@ WSGI_APPLICATION = 'library.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'library',
+        'USER': 'amir',
+        'PASSWORD': '9950',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
